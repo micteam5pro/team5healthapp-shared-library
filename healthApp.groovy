398c6363ebe44bedfda5ffe0a4099c1fe297391b
mmdpactv <michaelmathewmail@gmail.com>
2023-04-11 05:15:58 -0500

    new healthapp testing

--- healthApp.groovy
+++ healthApp.groovy
@@ -1,1 +1,32 @@
-#
+def call(String repoUrl){
+pipeline {
+       agent any
+       tools {
+           maven 'maven'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh 'mvn --version'
+                   sh 'java -version'
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage('team5healthapp-shared'){
+            steps{
+                echo "Welcome Engr michael app"
+            }
+           }
+           stage("to-test-maven") {
+               steps {
+                   sh 'mvn -v'
+               }
+           }
+       }
+}
+}
